3a7c4b064637d7460672c264cb23ba856ddc624e
Ell-716 <elenabai.2021@gmail.com>
2025-08-05 13:56:35 +0200

    feat(posts): add GET /posts endpoint with TDD

--- src/modules/posts/posts.routes.ts
+++ src/modules/posts/posts.routes.ts
@@ -5,11 +5,16 @@ import { CreatePostDto } from "./posts.types";
 const postsRoutes: FastifyPluginAsync = async (fastify: FastifyInstance) => {
   const service = postsService(fastify);
 
+  // POST /posts
   fastify.post<{ Body: CreatePostDto }>("/posts", async (request, reply) => {
     const newPost = await service.create(request.body);
-
-    // Return a 201 Created status code with the new post object
     return reply.code(201).send(newPost);
+  });
+
+  // GET /posts
+  fastify.get("/posts", async (request, reply) => {
+    const posts = await service.getAll();
+    return reply.code(200).send(posts);
   });
 };
 
--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -5,10 +5,13 @@ const postsService = (fastify: FastifyInstance) => {
   return {
     create: async (postData: CreatePostDto) => {
       fastify.log.info(`Creating a new post`);
-      // This will use the MOCK `transactions` in our test,
-      // and the REAL `transactions` in our live application.
       const post = fastify.transactions.posts.create(postData);
       return post;
+    },
+    getAll: async () => {
+      fastify.log.info(`Fetching all posts`);
+      const posts = fastify.transactions.posts.getAll();
+      return posts;
     },
   };
 };
