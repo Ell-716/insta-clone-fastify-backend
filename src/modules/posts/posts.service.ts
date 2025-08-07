91d4aca8f8481a8348b8bdb9430d0611adb47f28
Elena <elenabai.2021@gmail.com>
2025-08-07 13:03:31 +0200

    Update posts service to handle optional image uploads

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,11 +1,36 @@
 import type { FastifyInstance } from "fastify";
+import { fileStorageService } from "../../common/file-storage.service"; // Import the new service
 import { CreatePostDto } from "./posts.types";
 
-const postsService = (fastify: FastifyInstance) => {
+type CreatePostData = {
+  img_url: string; // This will now come from our storage service
+  caption: string;
+};
+
+type CreatePostServiceArgs = {
+  caption: string;
+  imageFile?: { buffer: Buffer; filename: string }; // New optional image file
+};
+
+export const postsService = (fastify: FastifyInstance) => {
   return {
-    create: async (postData: CreatePostDto) => {
+    create: async (data: CreatePostServiceArgs) => {
       fastify.log.info(`Creating a new post`);
-      const post = fastify.transactions.posts.create(postData);
+
+      let img_url = data.caption; // Fallback if no image, or placeholder
+
+      if (data.imageFile) {
+        // If an image is provided, save it and get the URL
+        img_url = await fileStorageService.saveImage(
+          data.imageFile.buffer,
+          data.imageFile.filename,
+        );
+      }
+
+      const post = fastify.transactions.posts.create({
+        img_url,
+        caption: data.caption,
+      });
       return post;
     },
     getAll: async () => {
@@ -15,5 +40,3 @@ const postsService = (fastify: FastifyInstance) => {
     },
   };
 };
-
-export { postsService };
