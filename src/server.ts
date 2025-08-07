0c79b51cea80bd76a9415bf509fb50a69d5782cf
Elena <elenabai.2021@gmail.com>
2025-08-07 16:52:32 +0200

    Add static image serving and test uploads in backend

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,27 @@
 import Fastify from "fastify";
+import path from "path";
+import fs from "fs/promises";
+import { randomUUID } from "crypto";
+import multipart from "@fastify/multipart";
+import fastifyStatic from "@fastify/static";
 import { databasePlugin } from "./core/database/database.plugin";
 import { postsRoutes } from "./modules/posts/posts.routes";
 import { reelsRoutes } from "./modules/reels/reels.routes";
 import { taggedRoutes } from "./modules/tagged/tagged.routes";
 import { highlightsRoutes } from "./modules/highlights/highlights.routes";
-import multipart from "@fastify/multipart";
-import fastifyStatic from "@fastify/static";
-import path from "path";
-import fs from "fs/promises";
-import { randomUUID } from "crypto";
 
 const fastify = Fastify({
   logger: true,
 });
 
-// Serve static files from /public/uploads/
+fastify.register(multipart);
+
+// Register static file serving
 fastify.register(fastifyStatic, {
-  root: path.join(process.cwd(), "public"),
-  prefix: "/uploads/",
+  root: path.join(__dirname, "../public"),
+  prefix: "/",
 });
 
-fastify.register(multipart);
 fastify.register(databasePlugin);
 fastify.register(postsRoutes);
 fastify.register(reelsRoutes);
@@ -32,7 +33,6 @@ fastify.get("/", function (request, reply) {
 });
 
 const port = 3000;
-
 fastify.listen({ port }, function (err, address) {
   if (err) {
     fastify.log.error(err);
