0d1112d065d5bd2bcf0cf3d0a93362ac5024636f
Elena <elenabai.2021@gmail.com>
2025-08-07 15:59:27 +0200

    Serve static files from /public/uploads using @fastify/static

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,19 @@ import { reelsRoutes } from "./modules/reels/reels.routes";
 import { taggedRoutes } from "./modules/tagged/tagged.routes";
 import { highlightsRoutes } from "./modules/highlights/highlights.routes";
 import multipart from "@fastify/multipart";
+import fastifyStatic from "@fastify/static";
 import path from "path";
 import fs from "fs/promises";
 import { randomUUID } from "crypto";
 
 const fastify = Fastify({
   logger: true,
+});
+
+// Serve static files from /public/uploads/
+fastify.register(fastifyStatic, {
+  root: path.join(process.cwd(), "public"),
+  prefix: "/uploads/",
 });
 
 fastify.register(multipart);
