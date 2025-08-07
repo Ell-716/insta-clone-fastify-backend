52457f68c3eaf89d4583b5c48ad143f3885deafc
Elena <elenabai.2021@gmail.com>
2025-08-07 12:36:06 +0200

    Setup file upload with @fastify/multipart and configure server for image handling

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,16 @@ import { postsRoutes } from "./modules/posts/posts.routes";
 import { reelsRoutes } from "./modules/reels/reels.routes";
 import { taggedRoutes } from "./modules/tagged/tagged.routes";
 import { highlightsRoutes } from "./modules/highlights/highlights.routes";
+import multipart from "@fastify/multipart";
+import path from "path";
+import fs from "fs/promises";
+import { randomUUID } from "crypto";
 
 const fastify = Fastify({
   logger: true,
 });
 
+fastify.register(multipart);
 fastify.register(databasePlugin);
 fastify.register(postsRoutes);
 fastify.register(reelsRoutes);
